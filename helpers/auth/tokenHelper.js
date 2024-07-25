42a5044b1e07fe02784a108fc2d2c2eff7f0f598
jewel109 <raihanurrahmanjewel@gmail.com>
2024-07-25 21:47:11 +0600

    token helper got refactored

--- helpers/auth/tokenHelper.js
+++ helpers/auth/tokenHelper.js
@@ -1,6 +1,7 @@
 const CustomError = require("../../middlewares/Error/CustomError");
+const { sendStatusError } = require("../httpError");
 
-const isTokenIncluded = (req, next) => {
+const isTokenIncluded = (req, res) => {
 
   // console.log(`in isTokenIncluded ${req}`)
   // console.log('in req.headers ' + req.headers.authrization)
@@ -8,30 +9,30 @@ const isTokenIncluded = (req, next) => {
 
   if (!req.headers.authorization) {
     //console.log("authorization in header is not found")
-    next("authrization in headers is not found")
+    sendStatusError(res, 404, "authrization in headers is not found")
   } else if (!req.headers.authorization.startsWith("Bearer")
   ) {
 
-    next("no token in authorization")
+    sendStatusError(res, 404, "no token in authorization")
   }
   return (req.headers.authorization && req.headers.authorization.startsWith("Bearer"))
 
 
 }
 
-const getAccessTokenFromHeader = (req, next) => {
+const getAccessTokenFromHeader = (req, res) => {
 
   const authorization = req.headers.authorization;
   // console.log(`authrization=${authrization}`)
   if (!authorization) {
-    next("not valid authorization")
+    sendStatusError(res, 404, "not valid authorization")
   }
   const accessToken = authorization.split(" ")[1];
   if (accessToken === "null") {
-    next("accessToken is null")
+    sendStatusError(res, 404, "accessToken is null")
   }
   if (!accessToken) {
-    next("accessToken is not found")
+    sendStatusError(res, 404, "accessToken is not found")
   }
   console.log(`access token ${accessToken}`)
   return accessToken;
@@ -39,6 +40,7 @@ const getAccessTokenFromHeader = (req, next) => {
 
 
 const sendToken = (user, statusCode, res) => {
+
   const token = user.generateJwtFromUser();
   // console.log(user)
   // console.log(token);
