96e173fad441292c651dc2ecfd374da530844c94
jewel109 <raihanurrahmanjewel@gmail.com>
2024-07-26 09:19:39 +0600

    new test added

--- __test__/post.spec.js
+++ __test__/post.spec.js
@@ -24,6 +24,9 @@ describe("create a post ", () => {
     // console.log('testing token ', process.env.TESTING_TOKEN)
     const response = await request(process.env.TESTING_URL).post("story/addstory").send({ title: "good", content: "very good raihanur rahman" }).set('Authorization', `Bearer ${process.env.TESTING_TOKEN}`)
 
+    // console.log(response.body.data.slug) // slug of the story
+    process.env.STORY_SLUG = response.body.data.slug
+    // console.log(process.env.STORY_SLUG)
     expect(response.body).toHaveProperty("success")
 
     expect(response.status).toBe(200)
@@ -32,3 +35,11 @@ describe("create a post ", () => {
 
 })
 
+describe("Get detail of a story", () => {
+
+  it("Should return 200 and show the post details", async () => {
+    const response = await request(process.env.TESTING_URL).post(`story/${process.env.STORY_SLUG}`).set('Authorization', `Bearer ${process.env.TESTING_TOKEN}`)
+    console.log(response.body)
+    expect(response.status).toBe(200)
+  })
+})
