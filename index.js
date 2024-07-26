c76b01bc816dee9baaf3c7f7c0a515c608a24d7a
jewel109 <raihanurrahmanjewel@gmail.com>
2024-07-26 08:06:36 +0600

    refactored index.js

--- index.js
+++ index.js
@@ -1,40 +1,49 @@
 const express = require("express")
 const dotenv = require('dotenv')
-const db = require("./helpers/db")
 const { connectingDB } = require("./helpers/db")
 const indexRoute = require('./routes/index')
 const cors = require("cors")
 const app = express()
 const logger = require('./helpers/logger/logger')
 const morgan = require("morgan")
+const captureResponseBody = require("./helpers/resmiddleware")
 
 //logger('we are in ', 'index.js')
 
 dotenv.config({
-  path: ".config/dev.env"
+  path: ".env"
 })
-// console.log(process.env.env)
-app.use(morgan(function(tokens, req, res) {
+console.log(process.env.PORT, process.env.TESTING_URL, process.env.JWT_SECRET)
+
+
+app.use(cors({
+  "Access-Control-Allow-Origin": "*"
+}))
+connectingDB()
+app.use(express.json())
+
+
+app.use(captureResponseBody);
+app.use(morgan(function (tokens, req, res) {
   return [
     tokens.method(req, res),
     tokens.url(req, res),
     tokens.status(req, res),
     tokens.res(req, res, 'content-length'), '-',
-    tokens['response-time'](req, res), 'ms'
+    tokens['response-time'](req, res), 'ms',
+    tokens.res(req, res, 'body'),
+    JSON.stringify(res.locals.body)
   ].join(' ')
 })
+
 )
-app.use(cors({
-  "Access-Control-Allow-Origin": "*"
-}))
-db()
-app.use(express.json())
-
-
-
-
 app.use('/', indexRoute)
 
+app.get('*', function (req, res) {
+  res.status(404).json({
+    message: "The route is missing"
+  });
+});
 app.use((err, req, res, next) => {
   console.error(`in index.js ${err.stack}`)
   next(err)
@@ -48,5 +57,4 @@ app.listen(port, () => {
   console.log(`server listening on port ${port}`)
 })
 
-
-module.exports = app
+module.exports = app 
