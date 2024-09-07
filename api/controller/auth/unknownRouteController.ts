6f2551dd378a2dbac251e97d9fe23a39c3c7ec63
jewel109 <raihanurrahmanjewel@gmail.com>
2024-09-07 11:55:07 +0600

    unknown route handler

---
+++ api/controller/auth/unknownRouteController.ts
@@ -0,0 +1,7 @@
+import { sendResponse, withRequest } from "../../../utils/controllerUtils";
+
+export const unKnownRouteController = withRequest(async (req, res) => {
+
+  console.log(req.url)
+  return sendResponse(res, { msg: "This route is not find" })
+})
