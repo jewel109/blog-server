082c8686632a93984c539311849587e376e9c81a
jewel109 <raihanurrahmanjewel@gmail.com>
2024-09-07 11:57:11 +0600

    elastic search test

--- tests/unit/elasticsearch.spec.ts
+++ tests/unit/elasticsearch.spec.ts
@@ -3,12 +3,14 @@ import { elasticSearchUri } from "../../utils/configUtils"
 
 
 console.log("node env ", process.env.NODE_ENV)
+const client = new Client({ node: elasticSearchUri })
+
 describe("Testing Elastic search db connection", () => {
 
 
   test("should connect to elastic db", async () => {
 
-    const client = new Client({ node: elasticSearchUri })
+
     expect(client).toBeDefined();
     const health = await client.cluster.health();
     expect(health).toHaveProperty('status');
