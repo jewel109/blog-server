9e15ae40c9f6d3aecfcfe410cd2bf0b6ffadb324
jewel109 <raihanurrahmanjewel@gmail.com>
2024-09-07 11:56:52 +0600

    auth test

--- tests/integration/auth.spec.ts
+++ tests/integration/auth.spec.ts
@@ -10,6 +10,7 @@ import { userModel } from '../../core/doamin/model/userModel'
 beforeAll(async () => {
 
   await mongoose.connect(String(mongoUrl))
+  await userModel.deleteMany({})
 })
 
 
@@ -49,4 +50,109 @@ describe("Handling register route request", () => {
 
   })
 
+})
+
+describe("Handling Login route", () => {
+
+  beforeAll(async () => {
+    await userModel.deleteMany({})
+  })
+
+  test("Sholud return success status with 201", async () => {
+
+    const { body, statusCode } = await request(url).post('/api/v1/register').send({ name: "jewel", email: 'jewel@gmail.com', password: "1253" })
+
+    const typeBody = body as ErrorResponseForTest
+
+    console.log(typeBody.msg)
+
+    expect(typeBody.msg).toBe(SUCCESS_RES_MSG)
+    expect(typeBody.status).toBe(SUCCESS_STATUS)
+    expect(statusCode).toBe(STATUS_CODE_201)
+    expect(typeBody.data).not.toBe(null)
+
+  })
+  test("should return success status having the token with 201 statusCode", async () => {
+    const { body, statusCode } = await request(url).post("/api/v1/login").send({ email: "jewel@gmail.com", password: "1253" })
+
+    const tBody = body as ErrorResponseForTest
+    console.log(tBody)
+    expect(tBody.msg).toBe(SUCCESS_RES_MSG)
+    expect(tBody.data.token).not.toBe(null)
+    expect(statusCode).toBe(STATUS_CODE_201)
+
+  })
+})
+
+describe("Middleware for accessing to private route", () => {
+
+  let token = ""
+  beforeAll(async () => {
+    const { body, statusCode } = await request(url).post("/api/v1/login").send({ email: "jewel@gmail.com", password: "1253" })
+
+    const tBody = body as ErrorResponseForTest
+    console.log(tBody)
+    expect(tBody.msg).toBe(SUCCESS_RES_MSG)
+    expect(tBody.data).not.toBe(null)
+    expect(statusCode).toBe(STATUS_CODE_201)
+    token = tBody.data
+    console.log("token ", tBody.data)
+
+  })
+
+  test("should have user data in the request object", async () => {
+
+    const { body, statusCode } = await request(url).post('/api/v1/private').set('Authorization', `bearer ${token}`)
+
+    const tBody = body as ErrorResponseForTest
+    expect(tBody.msg).toBe(SUCCESS_RES_MSG)
+    expect(tBody.data).not.toBe(null)
+    expect(statusCode).toBe(STATUS_CODE_200)
+    console.log(tBody)
+
+  })
+})
+
+describe("Handling following a user", () => {
+  let token = ''
+  beforeAll(async () => {
+
+    try {
+      await userModel.insertMany([
+        { name: "raihan", email: "raihan@gmail.com", password: '1234' }
+      ])
+      const { body, statusCode } = await request(url).post("/api/v1/login").send({ email: "jewel@gmail.com", password: "1253" })
+
+      const tBody = body as ErrorResponseForTest
+      // console.log(tBody)
+      expect(tBody.msg).toBe(SUCCESS_RES_MSG)
+      expect(tBody.data).not.toBe(null)
+      expect(statusCode).toBe(STATUS_CODE_201)
+      token = tBody.data
+      // console.log("token ", tBody.data)
+
+
+
+    } catch (error) {
+      console.log(error);
+
+    }
+  })
+  // when raihan will follow jewel 
+  // raihan will follow by sending request to server with followee name(raihan) and he's id(raihan)
+  // after finding the followee (jewel) by his name , he's id (raihan) will push to the following 
+  // array of followee(jewel)
+  // after successfull follow raihan will get a notification that he is following jewel
+  // and jewel will get notification that he is being followed by raihan
+  test("should have success status and 201 ", async () => {
+
+    const { body, statusCode } = await request(url).post('/api/v1/follow').send({ followeeEmail: 'raihan@gmail.com' }).set('Authorization', `bearer ${token}`)
+
+    const tBody = body as ErrorResponseForTest
+    expect(tBody.msg).toBe("You are now follwing ")
+    expect(tBody.data).not.toBe(null)
+    expect(statusCode).toBe(STATUS_CODE_201)
+
+
+  })
 })
