0101ffe8194972719239d0747c1ed0ee6ec0d55d
jewel109 <raihanurrahmanjewel@gmail.com>
2024-09-14 10:59:57 +0600

    middileware got refactored

--- api/middleware/middleware.ts
+++ api/middleware/middleware.ts
@@ -8,10 +8,16 @@ import { baseRouter } from "../router/baseRoute"
 
 export const setupMiddleware = async (app: Express) => {
 
-  const conn = await mongoose.connect(String(mongoUrl), {
-  });
-  app.use(express.json())
-  app.use(cors())
-  app.use(baseRouter)
-  app.use("*", unKnownRouteController)
+  try {
+    const conn = await mongoose.connect(String(mongoUrl), {
+    });
+    app.use(express.json())
+    app.use(cors())
+    app.use(baseRouter)
+    app.use("*", unKnownRouteController)
+
+  } catch (error) {
+    console.log(error)
+
+  }
 }
