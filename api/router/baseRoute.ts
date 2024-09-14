574b50a4597447b1d90c0f7cfc67d0dbd853e050
jewel109 <raihanurrahmanjewel@gmail.com>
2024-09-14 11:02:39 +0600

    notification route in base route

--- api/router/baseRoute.ts
+++ api/router/baseRoute.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { authRouter } from './auth/authRouter';
 import { followRouter } from './follow/followRouter';
+import { notificationRouter } from './notificatio/notificationRoute';
 
 export const baseRouter = express.Router();
 
 // Mount the authRouter on '/auth' path
 baseRouter.use('/api/v1', authRouter);
 baseRouter.use('/api/v1', followRouter)
+baseRouter.use('/api/v1', notificationRouter)
