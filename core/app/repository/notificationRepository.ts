d686274511f883a82e11738d9872053eac2d1835
jewel109 <raihanurrahmanjewel@gmail.com>
2024-09-18 09:57:00 +0600

    notification creation

--- core/app/repository/notificationRepository.ts
+++ core/app/repository/notificationRepository.ts
@@ -1,10 +1,18 @@
-import { createDefaultResponse, RepositoryResponse, tryCatchWithLogging } from "./repoUtils";
+import { Notifcation, NotificationI } from "../../doamin/model/notificationModel";
+import { createDefaultResponse, RepositoryResponse } from "./repoUtils";
 
 
 class NotificationRepository {
-  @tryCatchWithLogging
-  async create(): Promise<RepositoryResponse> {
-    return createDefaultResponse({ msg: "done" })
+  async create({ message, recipientId, read, recipientEmail, recipientName }: NotificationI): Promise<RepositoryResponse> {
+    try {
+      const data = await Notifcation.create({ message, recipientId, read, recipientName, recipientEmail })
+      if (!data) return createDefaultResponse({ msg: "Notification can't be create" })
+      return createDefaultResponse({ msg: "Notification created successfully", data })
+    } catch (error) {
+      const e = error as Error
+      console.log(error)
+      return createDefaultResponse({ msg: e.message })
+    }
   }
 }
 
